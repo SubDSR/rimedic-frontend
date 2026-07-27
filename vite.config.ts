518b2f9f6603217adeb2e91e36c4c87532ab71b6
SubDSR <davidsevanr@gmail.com>
2026-07-26 21:11:07 -0500

    chore(config): configuracion inicial del proyecto con Vite, TypeScript y ESLint

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test-setup.ts",
+  },
+});
